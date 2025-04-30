55ae22244ce7c9f790b6f5eb14e62250204eb1fa
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-04-30 23:32:47 +0530

    feat: routing in progress

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import Feed from'./components/Feed';
+import AdminLogin from'./components/AdminLogin';
+import StaffSignUp from'./components/StaffSignUp';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import StaffLogin from './components/StaffLogin';
+import Body from './components/Body';
+import Feed from'./components/Feed';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router';
+
+
+
+
 const AppLayout=()=>{
     return(
         <div>
-            <div>hello</div>
+           <Header/>
+           <Outlet/>
+           <Footer/>
         </div>
     )
 };
---
+++ src/components/StaffSignUp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StaffSignUp = () => {
+  return (
+    <div>StaffSignUp</div>
+  )
+}
+
+export default StaffSignUp
