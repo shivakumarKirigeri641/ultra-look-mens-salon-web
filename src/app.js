c21b828fb7de45ecde7909b177ec527252132ae4
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-04-30 23:23:15 +0530

    feat: structure done for git

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+const AppLayout=()=>{
+    return(
+        <div>
+            <div>hello</div>
+        </div>
+    )
+};
+ReactDOM.createRoot(document.getElementById('root')).render(<AppLayout/>);
