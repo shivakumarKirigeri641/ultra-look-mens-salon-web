14a6af831240c0243b109f6be57ae892360f4fa9
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-05-01 14:32:59 +0530

    fix: menu sub menu fix in progress, body content included.

--- src/app.js
+++ src/app.js
@@ -1,28 +1,24 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux';
 import StaffLogin from './components/StaffLogin';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import Feed from './components/Feed';
-import MenuBar from './components/MenuBar';
-import Error from './components/Error';
+import Body from './components/Body';
 import AdminLogin from './components/AdminLogin';
 import appStore from './store/appStore';
 import { BrowserRouter, createBrowserRouter, Outlet, Route, Routes } from 'react-router';
+import Body from './components/Body';
 const AppLayout=()=>{
     return(
-        <div>
-            <Provider store={appStore}>
+        <div>            
+            <Provider store={appStore}>                                
                 <BrowserRouter basename='/'>
-                    <Header/>
-                    <MenuBar/>
-                    <Routes>
-                        <Route path='/' element={<Feed/>}>
+                    <Routes>                        
+                        <Route path='/' element={<Body/>}>
+                            <Route path='/' element={<Feed/>}/>
                             <Route path='/stafflogin' element={<StaffLogin/>}/>
                             <Route path='/adminlogin' element={<AdminLogin/>}/>
                         </Route>
                     </Routes>
-                    <Footer/>
                 </BrowserRouter>
             </Provider>
         </div>
---
+++ src/components/Body.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header';
+import Footer from './Footer';
+import MenuBar from './MenuBar';
+import { Outlet } from 'react-router'
+
+const Body = () => {
+  return (
+    <div>
+        <Header/>
+        <MenuBar/>
+        <Outlet/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Body
